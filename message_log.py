bd26daa0516e6c52f2a14cb686d934b15b5263ff
Eregul <153775012+Eregul@users.noreply.github.com>
2025-12-23 10:33:59 -0800

    Part Seven
    RLT P7

---
+++ message_log.py
@@ -0,0 +1,61 @@
+from typing import List, Reversible, Tuple
+import textwrap
+
+import tcod
+
+
+import color
+
+
+class Message:
+    def __init__(self, text:str, fg:Tuple[int, int, int]):
+        self.plain_text = text
+        self.fg = fg
+        self.count = 1
+
+    @property
+    def full_text(self) -> str:
+        # Full text of message, including count if needed
+        if self.count > 1:
+            return f"{self.plain_text} (x{self.count})"
+        return self.plain_text
+    
+class MessageLog:
+    def __init__(self) -> None:
+        self.messages: List[Message] = []
+
+    def add_message(
+        self, text:str, fg:Tuple[int, int, int] = color.white, *, stack:bool = True,
+    ) -> None:
+        # Add message to log. 'Text' is message; 'fg' is text color.
+        # If 'stack' is True message stacks with previous message of same text.
+        if stack and self.messages and text == self.messages[-1].plain_text:
+            self.messages[-1].count += 1
+        else:
+            self.messages.append(Message(text, fg))
+
+    def render(
+        self, console:tcod.console.Console, x:int, y:int, width:int, height:int,
+    ) -> None:
+        # Render log over area x, y, width, height
+        self.render_messages(console, x, y, width, height, self.messages)
+
+    @staticmethod
+    def render_messages(
+        console:tcod.console.Console,
+        x:int,
+        y:int,
+        width:int,
+        height:int,
+        messages:Reversible[Message],
+    ) -> None:
+        # Render messages starting at most recent
+        y_offset = height - 1
+
+        for message in reversed(messages):
+            for line in reversed(textwrap.wrap(message.full_text, width)):
+                console.print(x=x, y=y + y_offset, string=line, fg=message.fg)
+                y_offset -= 1
+                if y_offset < 0:
+                    return # Out of space
+                
